2e4597185f25619f109942131e0fa5c96ce63245
jakubkrhovjak <c_jkrhovjak@groupon.com>
2025-11-21 16:42:34 +0100

    java21

--- rigle-java/src/main/java/com/rigle/Item.java
+++ rigle-java/src/main/java/com/rigle/Item.java
@@ -1,12 +1,15 @@
 package com.rigle;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "item")
-public class Item extends PanacheEntity {
+public class Item extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
     public String name;
     public String description;
 }
